a4134b20a8e29f852e99933a155d0b6d71a847ad
Guillaume Besson <guillaume@besson.co>
2020-04-29 13:36:01 +0200

    Reactivate arm building

--- build_scripts/build_for_arch.sh
+++ build_scripts/build_for_arch.sh
@@ -15,7 +15,11 @@ docker build ../ --file ./Dockerfile_crossarch --tag crossbuild
 
 docker run --rm --workdir /workspace \
   -v `realpath ../`:/workspace \
-  crossbuild bash -c "autoreconf -i -f && \
-  ./configure --with-avahi --with-ssl=openssl --with-metadata --with-stdout --with-tinysvcmdns && \
-  make"
+  crossbuild bash -c "\
+  autoreconf -i -f && \
+  ./configure --with-ssl=openssl --with-metadata --with-stdout --with-tinysvcmdns --with-avahi && \
+  make && \
+  mkdir shairport_with_libs && \
+  find \`ldd ./shairport-sync | sed -En \"s/(libpopt|libconfig|libavahi-common|libavahi-client)\.so.*\=> (.*) \(.*/\2/p\"\` -exec cp -- {} ./shairport_with_libs/ \; && \
+  cp ./shairport-sync ./shairport_with_libs"
 
